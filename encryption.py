7b81d2904ee65cadb40851d1de500a62599a59c6
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2019-05-15 11:35:58 -0400

    added file select GUI

--- encryption.py
+++ encryption.py
@@ -12,6 +12,8 @@ Data Dictionary
 main
 crypt
 location
+selected_file
+raw_file
 """
 
 # This class generates the GUI for the program and runs the main menu
@@ -30,7 +32,7 @@ class MainMenu(tk.Frame):
         self.main.rowconfigure(0, weight=1)
         self.main.columnconfigure(0, weight=1)
         
-        # Add text and options
+        # Add intro and buttons to encrypt/decrypt/quit program
         tk.Label(self.main, text='Welcome to the Encryption program! This program will take a text file that you specify, and '+
                  'then either encrypt or decrypt its data with the provided password. Select an option to the right to get started.',
                  wraplength=240, height=5, justify='left').grid(rowspan=3, row=0, columnspan=2, column=0, padx=(0,5))
@@ -41,12 +43,12 @@ class MainMenu(tk.Frame):
 
     # This function calls the EncryptMenu class to encrypt data.
     def encrypt(self):
-        crypt = CryptMenu(self, 'en')
+        crypt = CryptMenu(self, 'En')
     # End encrypt
 
     # This function calls the DecryptMenu class to decrypt data.
     def decrypt(self):
-        crypt = CryptMenu(self, 'de')
+        crypt = CryptMenu(self, 'De')
     # End decrypt
 # End MainMenu
 
@@ -59,15 +61,35 @@ class CryptMenu(tk.Toplevel):
     def __init__(self, main, action):
         # Initialize window
         tk.Toplevel.__init__(self, main)
+        self.attributes('-topmost', 'true')
         self.main = main
-        self.title('Encrypt Data')
+        self.action = action
+        self.title('{}crypt Data'.format(self.action))
         
-        # Add file select menu
-        tk.Label(self, text='Please select the location of the file you would like to encrypt.').grid(columnspan=3,
+        # Add intro, text box to enter file address, and buttons to select with GUI and OK
+        tk.Label(self, text='Please type or select the location of the file you would like to {}crypt below.'.format(self.action.lower())).grid(columnspan=3,
                                                                                                                   column=0, row=0)
-        tk.Label(self, text='File location:', anchor='w', borderwidth=1, relief='solid').grid(column=0, row=1, padx=(0,5), sticky='W')
-        location = tk.Entry(self, width=60).grid(columnspan=2, column=0, row=1, padx=(70,0), sticky='NSEW')
-# End EncryptMenu
+        tk.Label(self, text='File\nlocation:', anchor='w', borderwidth=1, relief='solid').grid(column=0, rowspan=2, row=1, padx=(0,5), sticky='NSW')
+        self.location = tk.Text(self, wrap=tk.WORD, height=3, width=36)
+        self.location.grid(column=0, rowspan=2, row=1, padx=(50,0))
+        tk.Button(self, text='Open file', command=self.file_select, width=8).grid(column=1, row=1, sticky='NSEW')
+        tk.Button(self, text='Use this file', command=self.open_file, width=8).grid(column=1, row=2, sticky='NSEW')
+    # End __init__
+    
+    # This function uses tkFileDialog to allow the user to select a specific file.
+    def file_select(self):
+        # Open GUI to select file
+        selected_file = tkFileDialog.askopenfilename(initialdir='H:/', title = 'Select File', filetypes=(('Text Documents', '*.txt'), ('Markdown Document', '*.md'),
+                                                                                                         ('Rich Text Format', '*.rtf'), ('All Files', '*.*')))
+        
+        # Replace text box contents with selected file
+        self.location.delete('1.0', tk.END)
+        self.location.insert(tk.END, selected_file)
+        self.lift() # Needs to lift after GUI file select closes
+                
+    def open_file(self):
+        self.raw_file = self.location.get('1.0', 'end-1c')
+# End CryptMenu
 
 a = MainMenu()
 a.mainloop()
